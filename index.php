b00ca25bbed704b3cd75cfd3199b8c176db973d2
MaTtiaa01 <mattia2001@live.it>
2022-12-12 16:15:06 +0100

    style product card

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require_once __DIR__ . '/Models/Product.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Pet E-commerce </title>
 </head>
 
@@ -20,11 +21,12 @@ require_once __DIR__ . '/Models/Product.php';
     <?php if (count($products) > 0) : ?>
         <section class="products">
             <div class="container">
-                <div class="row">
+                <h1 class="text-center my-5">Pet Products</h1>
+                <div class="row g-5">
                     <?php foreach ($products as $product) : ?>
                         <div class="col">
-                            <div class="card">
-                                <div class="card-head">
+                            <div class="card p-3">
+                                <div class="card-head text-center">
                                     <img src="<?= $product->img ?>" alt="">
                                 </div>
                                 <div class="card-body">
