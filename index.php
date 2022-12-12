c84f29dd764da6b05a6b99dc6410801112e731f0
MaTtiaa01 <mattia2001@live.it>
2022-12-12 16:33:10 +0100

    add icons

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require_once __DIR__ . '/Models/Product.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Pet E-commerce </title>
 </head>
 
@@ -38,7 +39,12 @@ require_once __DIR__ . '/Models/Product.php';
                                             <strong>PRICE:</strong> <?= $product->price ?> €
                                         </div>
                                         <div class="category">
-                                            <strong>CATEGORY:</strong> <?= $product->category ?>
+                                            <strong>CATEGORY:</strong>
+                                            <?php if ($product->category === "dog") : ?>
+                                                <i class="fa-solid fa-dog"></i>
+                                            <?php else : ?>
+                                                <i class="fa-solid fa-cat"></i>
+                                            <?php endif; ?>
                                         </div>
                                         <div class="type">
                                             <strong>TYPE:</strong> <?= $product->type ?>
