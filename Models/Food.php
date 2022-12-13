da952e1452d976b8b100c9514f15ecb9021518fb
MaTtiaa01 <mattia2001@live.it>
2022-12-13 15:35:48 +0100

    remove some model user, add toy and food models

---
+++ Models/Food.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Food extends Product
+{
+    public function __construct(String $name, String $category, String $title, String $img, Int $price, String $type, protected Float $weight)
+    {
+        parent::__construct($name, $category, $title, $img, $price, $type);
+        $this->weight = $weight;
+    }
+}
